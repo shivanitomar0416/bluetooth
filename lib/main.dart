fad93083b9469e43ac7c228af6f1e2710cdc6747
shivani tomar <shivanitomar7409@gmail.com>
2024-10-11 23:57:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("BLE SCANNER"),),
+        body: GetBuilder<BleController>(
+          init: BleController(),
+          builder: (BleController controller)
+          {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  StreamBuilder<List<ScanResult>>(
+                      stream: controller.scanResults,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Expanded(
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data!.length,
+                                itemBuilder: (context, index) {
+                                  final data = snapshot.data![index];
+                                  return Card(
+                                    elevation: 2,
+                                    child: ListTile(
+                                      title: Text(data.device.name),
+                                      subtitle: Text(data.device.id.id),
+                                      trailing: Text(data.rssi.toString()),
+                                      onTap: ()=> controller.connectToDevice(data.device),
+                                    ),
+                                  );
+                                }),
+                          );
+                        }else{
+                          return Center(child: Text("No Device Found"),);
+                        }
+                      }),
+                  SizedBox(height: 10,),
+                  ElevatedButton(onPressed: ()  async {
+                    controller.scanDevices();
+                    // await controller.disconnectDevice();
+                  }, child: Text("SCAN")),
+
+                ],
+              ),
+            );
+          },
+        )
+    );
+  }
+}
